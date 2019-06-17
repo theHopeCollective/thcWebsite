6711f06b76f4b22d298c7438fe514abc5e60ffba
theHopeCollective <thehopecollective919@gmail.com>
2019-06-17 15:18:39 -0400

    2nd commit

--- thcWebsite/thcWebsite/settings.py
+++ thcWebsite/thcWebsite/settings.py
@@ -14,7 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR= os.path.join(BASE_DIR, "static")
+MEDIA_DIR= os.path.join(BASE_DIR, "media")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'thcStore',
+    'accounts',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'thcWebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,10 +80,15 @@ WSGI_APPLICATION = 'thcWebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'thcdb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
@@ -118,3 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS= [STATIC_DIR,]
+
+#MEDIA
+MEDIA_ROOT= MEDIA_DIR
+MEDIA_URL= '/media/'
+
+LOGIN_URL = '/accounts/user_login'
+LOGIN_REDIRECT_URL= 'test'
+LOGOUT_REDIRECT_URL= 'thanks'
--- thcWebsite/thcWebsite/urls.py
+++ thcWebsite/thcWebsite/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import thcGeneral
+
+from . import views
 
 urlpatterns = [
+
+    path('', views.HomePage.as_view(), name= 'home'),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('test/', views.TestPage.as_view(), name= 'test'),
+    path('thanks/', views.ThanksPage.as_view(), name= 'thanks')
+
 ]
