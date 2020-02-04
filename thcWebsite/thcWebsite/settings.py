73153d4e22eb43bbc3d4d3ee96520100c867778c
theHopeCollective <thehopecollective919@gmail.com>
2020-02-03 19:40:17 -0500

    master.css working

--- thcWebsite/thcWebsite/settings.py
+++ thcWebsite/thcWebsite/settings.py
@@ -146,13 +146,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS= [STATIC_DIR,]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS= [
+STATIC_DIR,
+]
 
 #MEDIA
 MEDIA_ROOT= MEDIA_DIR
 MEDIA_URL= '/media/'
 
+#LOGIN 
 LOGIN_URL = '/accounts/user_login'
 LOGIN_REDIRECT_URL= 'test'
 LOGOUT_REDIRECT_URL= 'thanks'
--- thcWebsite/thcWebsite/urls.py
+++ thcWebsite/thcWebsite/urls.py
@@ -18,8 +18,6 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
-import thcGeneral
-
 from . import views
 
 urlpatterns = [
